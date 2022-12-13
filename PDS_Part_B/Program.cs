9e097e78152aa659ddb5912ed997c6a6b3f0893b
MiroslavaRo <miroslavaroshchina@gmail.com>
2022-12-13 05:49:51 +0200

    option 2

--- PDS_Part_B/Program.cs
+++ PDS_Part_B/Program.cs
@@ -10,12 +10,12 @@ namespace PDS_Part_B
 {
     internal class Program
     {
-        public static int amount = 20_00;
+        public static int amount = 50_00;
         public static int circlesAmount = 1000; //the number of the circles
         public static int paintingTime = 20; //20 msec
         static readonly object locker = new object();
-        //  public static int[] k = { 5, 20, 100 }; //number of wiorkers
-        public static int k = 5;
+        public static int[] workers = {1, 5, 20, 100 }; //number of workers
+       // public static int k = 20;
 
         static void Main(string[] args)
         {
@@ -27,19 +27,48 @@ namespace PDS_Part_B
         static void SectionB()
         {
             var circleList = GenerateCoordinates();
+            var temp = circlesAmount;
+
 
             var stopwatch = new Stopwatch();
+            for (var i = 0; i < workers.Length; i++)
+            {
+                circlesAmount= temp;
+                var k = workers[i];
+                Console.WriteLine("START");
+                stopwatch.Restart();
 
-            Console.WriteLine("START");
-            stopwatch.Start();
-            //Console.WriteLine($"Coordinates: \n{string.Join("; ", circleList)}");
+                ThreadsWork01(circleList, k);
+                stopwatch.Stop();
+                Console.WriteLine("---------------------------------------");
 
-            ThreadsWork(circleList);
+                Console.Write($"Time to impliment with {k} workers (Option 1): ");
+                Console.Write($"{stopwatch.ElapsedMilliseconds} miliseconds\n");
 
-            Console.WriteLine("---------------------------------------");
-            stopwatch.Stop();
-            Console.Write($"Time to impliment with {k} workers: ");
-            Console.Write($"{stopwatch.ElapsedMilliseconds} miliseconds\n");
+                Console.Write($"Press enter to continue...\n");
+
+                Console.ReadLine();
+
+
+                //--------------------------------------------------------------//
+                stopwatch.Reset();
+                stopwatch.Restart();
+
+
+                Console.WriteLine("START");
+                circlesAmount = temp;
+                ThreadsWork02(circleList, k);
+                Console.WriteLine("---------------------------------------");
+                stopwatch.Stop();
+                Console.Write($"Time to impliment with {k} workers (Option 2): ");
+                Console.Write($"{stopwatch.ElapsedMilliseconds} miliseconds\n");
+
+                Console.ReadLine();
+
+            }
+            
+
+
         }
 
         static string[] GenerateCoordinates()
@@ -57,22 +86,20 @@ namespace PDS_Part_B
 
             return coordinates;
         }
-        static void ThreadsWork(string[] circleList)
+        static void ThreadsWork01(string[] circleList, int k)
         {
-            var stopwatch1 = new Stopwatch();
-            stopwatch1.Start();
-            var n = circlesAmount;
-            
+           // var n = circlesAmount;           
 
             IDictionary<int, List<string>> workerLists = new Dictionary<int, List<string>>();
-           
+            var lastIndexes = new List<int>();
+            lastIndexes.Add(0);
 
             //start threads
             var threads = new List<Thread>();
 
-            for (int i = 0; i < k-1; i++)
+            for (int i = 0; i < k; i++)
             {
-                var thread = new Thread(() => PaintCircle(circleList, workerLists));
+                var thread = new Thread(() => PaintCircleMax(circleList, workerLists, lastIndexes));
                 thread.Start();
                 threads.Add(thread);
             }
@@ -85,58 +112,137 @@ namespace PDS_Part_B
 
 
             var sum = 0;
-            Console.WriteLine("\n AMOUNT:" + circlesAmount);
             foreach (var kvp in workerLists)
             {
-                Console.WriteLine($"\n---------------WorkerList {kvp.Key}--------------");
-                Console.WriteLine(string.Join("\n", kvp.Value));
-                Console.WriteLine($"Length: {kvp.Value.Count}");
+             //   Console.WriteLine($"Length: {kvp.Value.Count}");
                 sum+= kvp.Value.Count;
             }
-            Console.WriteLine($"\nFULL LENGTH: {sum}");
+          //  Console.WriteLine($"\nFULL LENGTH: {sum}");
+
+        }
+
+        static void ThreadsWork02(string[] circleList2, int k)
+        {
+            //var n = circlesAmount;
+
+            IDictionary<int, List<string>> workerLists = new Dictionary<int, List<string>>();
+            var lastIndexes = new List<int>();
+            lastIndexes.Add(0);
+
+
+            //start threads
+            var threads = new List<Thread>();
+
+            for (int i = 0; i < k; i++)
+            {
+                var thread = new Thread(() => PaintCircleAsk(circleList2, workerLists));
+                thread.Start();
+                threads.Add(thread);
+            }
+
+            //stop threads
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+
+            var sum = 0;
+            foreach (var kvp in workerLists)
+            {
+               // Console.WriteLine($"\n---------------WorkerList {kvp.Key}--------------");
+              //  Console.WriteLine(string.Join("\n", kvp.Value));
+                //Console.WriteLine($"Length: {kvp.Value.Count}");
+                sum += kvp.Value.Count;
+            }
+           // Console.WriteLine($"\nFULL LENGTH: {sum}");
 
         }
 
 
 
 
-        static void PaintCircle(string[] circleList, IDictionary<int, List<string>> workerLists)//List<string>[] workerLists)
+        //Option 1 (the circle with largest index inserted)
+        static void PaintCircleMax(string[] circleList, IDictionary<int, List<string>> workerLists, List<int> lastIndexes)
         {
             var thread = Thread.CurrentThread;
             var contain = false;
 
             //itarating the dictionary for current worker
             var id = thread.ManagedThreadId;
+
             lock (locker) {
                 workerLists.Add(id, new List<string>());
             }
 
             for (var i = 0; i < circleList.Length; i++)
             {
+                
                 //access to circle with index i
                 lock (locker)
                 {
-                    Thread.Sleep(10);
                     if (circlesAmount <= 0)
                     {
                         break;
                     }
-                    Console.WriteLine($"Circle {i}: {circleList[i]} for {id}");
+                    var max = lastIndexes.Last();
+                    Thread.Sleep(paintingTime);
+                    i = max = max+1;
+                    workerLists[id].Add(circleList[i]);
+                    //Console.WriteLine($"Circle {i} for {id} Painting....\n");
+                    
+                    lastIndexes.Add(max);
+                    
+
+                    circlesAmount--;
+
+                }
+            }
+        }
+
+
+
+        //Option 2 (ask do list of values contains current coordinates)
+        static void PaintCircleAsk(string[] circleList, IDictionary<int, List<string>> workerLists)
+        {
+            var thread = Thread.CurrentThread;
+            var contain = false;
+
+            //itarating the dictionary for current worker
+            var id = thread.ManagedThreadId;
+
+            lock (locker)
+            {
+                workerLists.Add(id, new List<string>());
+            }
+
+            for (var i = 0; i < circleList.Length; i++)
+            {
+
+                //access to circle with index i
+                lock (locker)
+                {
+                    if (circlesAmount <= 0)
+                    {
+                        break;
+                    }
+
+                   // Console.WriteLine($"Circle {i}: {circleList[i]} for {id}");
                     foreach (var kvp in workerLists)
                     {
                         if (kvp.Value.Contains(circleList[i]))
                         {
                             contain = true;
-                            Console.WriteLine("=>Already exists.\n");
+                           // Console.WriteLine("=>Already exists.\n");
                             break;
 
                         }
                     }
                     if (contain == false)
                     {
-                        Thread.Sleep(200);
+                        Thread.Sleep(paintingTime);
                         workerLists[id].Add(circleList[i]);
-                        Console.WriteLine("Painting....\n");
+                      //  Console.WriteLine("Painting....\n");
                     }
                     else
                     {
